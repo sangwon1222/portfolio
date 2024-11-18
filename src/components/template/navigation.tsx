bba3026c949680687124200cb132e717ddaf4f09
lsw <hkbvc1222@gmail.com>
2024-11-18 15:56:35 +0900

    feat: 블로그 네비게이션 추가

--- src/components/template/navigation.tsx
+++ src/components/template/navigation.tsx
@@ -12,6 +12,7 @@ const btnList = [
   { label: "About", href: "/#about" },
   { label: "Experience", href: "/#experience" },
   { label: "Game", href: "/game" },
+  { label: "Blog", href: "https://velog.io/@hkbvc1222/posts" },
 ];
 
 const Navigation: React.FC = memo(() => {
