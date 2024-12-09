047dd08c7c35f7b2b994645b8b6a8871f8c666ac
lsw <hkbvc1222@gmail.com>
2024-12-09 17:34:50 +0900

    style: 소개/이력 레이아웃 통일

--- src/components/templates/About.tsx
+++ src/components/templates/About.tsx
@@ -9,28 +9,33 @@ export default function About() {
     <>
       <div id="about" className="h-20" />
       <div className="flex flex-col items-center gap-10 px-2 w-full ">
+        {/* Title */}
         <h3 className="px-2 m-auto w-fit text-center relative after:contents-[''] after:block after:absolute after:w-full after:h-2 after:bg-red-400 after:z-[1] after:left-0 after:bottom-0 after:animate-pulse animate-pulse">
           <span className="relative z-[2] py-6">{t('about').toUpperCase()}</span>
         </h3>
-        <ul className="flex flex-col gap-5 p-3 desktop:p-10 tablet:p-7 border min-w-[320px] max-w-[700px] w-full">
-          <li className="flex flex-col gap-3">
-            <p className="font-bold text-xl">
-              {t('aboutMe')}
-              <br /> <span className="text-main-2">{t('myName')}</span>
-              {currentLocale == 'ko' && '입니다.'}
-            </p>
 
-            <p></p>
-          </li>
+        {/* 소개 글 */}
+        <div className="min-w-[320px] max-w-[700px] w-full p-2 desktop:p-2 tablet:p-7">
+          <ul className="flex flex-col gap-5 border p-2">
+            <li className="flex flex-col gap-3">
+              <p className="font-bold text-xl">
+                {t('aboutMe')}
+                <br /> <span className="text-main-2">{t('myName')}</span>
+                {currentLocale == 'ko' && '입니다.'}
+              </p>
 
-          <li>
-            <ul className="list-disc px-5 font-normal">
-              <li>{t('aboutMeSub1')}</li>
-              <li>{t('aboutMeSub2')}</li>
-              <li>{t('aboutMeSub3')}</li>
-            </ul>
-          </li>
-        </ul>
+              <p></p>
+            </li>
+
+            <li>
+              <ul className="list-disc px-5 font-normal">
+                <li>{t('aboutMeSub1')}</li>
+                <li>{t('aboutMeSub2')}</li>
+                <li>{t('aboutMeSub3')}</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/components/templates/Experience.tsx
+++ src/components/templates/Experience.tsx
@@ -10,9 +10,12 @@ export default function Experience() {
     <>
       <div id="experience" className="h-20" />
       <div className="flex flex-col items-center gap-10 px-2 w-full">
+        {/* Title */}
         <h3 className="px-2 m-auto w-fit text-center relative after:contents-[''] after:block after:absolute after:w-full after:h-2 after:bg-red-400 after:z-[1] after:left-0 after:bottom-0 after:animate-pulse animate-pulse">
           <span className="relative z-[2] py-6">{t('experience').toUpperCase()}</span>
         </h3>
+
+        {/* 이력 나열 */}
         <div className="flex flex-col gap-5 p-2 desktop:p-2 tablet:p-7 min-w-[320px] max-w-[700px] w-full">
           <Grid1Ratio2 label={t('sonid')} period="2023.03 ~ 2024.07" href="http://www.sonid.co.kr/">
             <ProjectCard
